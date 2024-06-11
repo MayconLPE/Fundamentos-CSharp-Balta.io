afd87963a4c6f26f2e1b5466552cfb4f6621a5a3
MayconL27 <mayconleandro2008@gmail.com>
2024-06-11 07:51:33 -0300

    Guids

---
+++ Guids/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var id = Guid.NewGuid();
+        id.ToString();
+
+        id = new Guid("b6b03683-977a-42c7-8957-4a11cf6e37c5");
+
+        // if(id == Guid.NewGuid())
+        // id = new Guid(); cria guild com tudo zero
+
+        Console.WriteLine(id.ToString().Substring(0, 8));
+    }
+}
