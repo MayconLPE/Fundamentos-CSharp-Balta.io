a89806523166797ca5a6c83aa31bf7265c2c6130
MayconL27 <mayconleandro2008@gmail.com>
2024-06-16 11:00:01 -0300

    Concluido

---
+++ ComparacaoStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var texto = "teste";
+        Console.WriteLine(texto.CompareTo("Teste"));
+        Console.WriteLine(texto.Contains("teste"));
+        Console.WriteLine(texto.Contains("Teste"));
+        Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+
+
+        var texto1 = "Este texto é um teste";
+        Console.WriteLine(texto1.StartsWith("Este"));
+        Console.WriteLine(texto1.StartsWith("este", StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine(texto1.StartsWith("texto"));
+
+        Console.WriteLine(texto.EndsWith("teste"));
+        Console.WriteLine(texto.Equals("teste", StringComparison.OrdinalIgnoreCase));
+
+        Console.WriteLine(texto1.IndexOf("e"));
+        Console.WriteLine(texto1.LastIndexOf("e"));
+
+        Console.WriteLine(texto1.ToUpper());
+        Console.WriteLine(texto1.ToLower());
+        Console.WriteLine(texto1.Insert(5, " Aqui "));
+        Console.WriteLine(texto1.Remove(5, 5));
+        Console.WriteLine(texto1.Length);
+
+
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        var arr = new int[3];
+
+        try
+        {
+            // for (var index = 0; index < 4; index++)
+            // {
+            // IndexOutOfRangerException
+            //      Console.WriteLine(arr[index]);
+            //   }
+
+            Cadastrar("");
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Não encontrou o índice na lista!");
+
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Falha ao cadastrar texto");
+
+        }
+        catch (Exception ex) // para erros desconhecidos ou genericos.
+        {
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Algo deu errado!");
+        }
+        finally
+        {
+            Console.WriteLine("Chegou ao fim!");
+        }
+    }
+
+    private static void Cadastrar(string texto)
+    {
+        if (string.IsNullOrEmpty(texto))
+            throw new Exception("O texto não pode ser nulo ou vazio");
+    }
+}
